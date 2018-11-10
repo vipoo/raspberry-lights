c5049cce7cde81d5a4f1199abca3dc51e5605c4d
Dean Netherton <dean.netherton@gmail.com>
2018-11-10 21:03:13 +1100

    save power - turn off hdmi

--- app/power-saving.mjs
+++ app/power-saving.mjs
@@ -1,7 +1,7 @@
 import fs from 'fs'
+import {exec} from 'lib/exec'
 
 export default function main() {
   fs.writeFileSync('/sys/devices/platform/soc/20980000.usb/buspower', '0x0')
+  exec('vcgencmd display_power 0')
 }
-
-//exec('sudo sh -c 'echo 0x0 > /sys/devices/platform/soc/20980000.usb/buspower'')
